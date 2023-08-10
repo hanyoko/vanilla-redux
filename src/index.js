9006f16a6f0b937aa44659cc350ade0d4f5e7b75
yohan <koyohanyoko@gmail.com>
2023-08-10 19:21:16 +0900

    DAY 2

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const { legacy_createStore } = require("redux");
+
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const countModifier = (state = 0) => {
+  console.log(state);
+  return state;
+}
+
+const countStore = legacy_createStore(countModifier);
+
+console.log(countStore.getState());
