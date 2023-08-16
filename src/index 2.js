219c089ada3fa67cd74c5dec03cc86c4f844fe6a
yohan <koyohanyoko@gmail.com>
2023-08-16 18:33:18 +0900

    DAY 3

--- src/index 2.js
+++ src/index 2.js
@@ -5,7 +5,7 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
+const countModifier = (state = 0) => {
   console.log(state);
   return state;
 }
